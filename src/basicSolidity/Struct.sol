d8cee1c2c342c950f1e47cd6962040ba1af263a1
peryGoldberg <pe0533183564@gmail.com>
2024-06-16 16:11:36 +0300

    commit23

--- src/basicSolidity/Struct.sol
+++ src/basicSolidity/Struct.sol
@@ -1,10 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+
 contract toDo{
 struct todo{
     string public text;
     bool complete;
 }
 
+todo[] public Todo;
+
+function create(string calldata _text) public{
+Todo.push(Todo(_text,false));
+Todo.push(Todo({text:_text,complete:false}));
+
+todo memory t;
+t.text=_text;
+Todo.push(t);
+
+}
+
+function get(uint _index) public view returns(string text, bool complete){
+   todo public storage t= Todo[_index];
+   return(t.text,t.complete);
+}
+
+function appdateText(uint _index,string _text) public{
+    Todo[_index].text=_text;
+}
+
+function appdateComplete(uint _index) public{
+    Todo[_index].complete=!Todo[_index].complete;
+}
 }
--- src/staking/Staking.sol
+++ src/staking/Staking.sol
@@ -1,71 +1,70 @@
 
-// // SPDX-License-Identifier: MIT
-// // https://solidity-by-example.org/defi/staking-rewards/
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "../../newProject/src/ERC20.sol";
-//  pragma solidity ^0.8.20;
-// struct userDeposit{
-//   uint time;
-//   uint amount;
-// }
-// contract Staking{
-//  address public owner;
-//  MyToken public immutable myToken ;
-//  uint public totalSupply;
-//  //address=> time=>amount the user depisited
-//  mapping(address=>mapping(uint=>userDeposit)) public deposit;
-//  mapping(address=>uint) public numOfDeposit;
+// SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../../newProject/src/ERC20.sol";
+ pragma solidity ^0.8.20;
+struct userDeposit{
+  uint time;
+  uint amount;
+}
+contract Staking{
+ address public owner;
+ MyToken public immutable myToken ;
+ uint public totalSupply;
+ //address=> time=>amount the user depisited
+ mapping(address=>mapping(uint=>userDeposit)) public deposit;
+ mapping(address=>uint) public numOfDeposit;
 
-//   constructor(address token) {
-//         owner = msg.sender;
-//         myToken = MyToken(token);
-//         totalSupply=1000000;
-//         myToken.mint(address(this),totalSupply);
-//   }
+  constructor(address token) {
+        owner = msg.sender;
+        myToken = MyToken(token);
+        totalSupply=1000000;
+        myToken.mint(address(this),totalSupply);
+  }
  
-//   modifier reward(address user){
-//        require(user!=address(0),"Address does not make sense");
-//        _;
-//   }
-// //A function that deposits money
-//   function deposite(uint amount) external reward(msg.sender){
-//         require(amount>0,"The amount must be greater than 0");
-//         deposit[msg.sender][block.timestamp]=amount;
-//         myToken.transfer(address(this), amount);
-//         numOfDeposit[msg.sender]+=1;
-//   }
+  modifier reward(address user){
+       require(user!=address(0),"Address does not make sense");
+       _;
+  }
+//A function that deposits money
+  function deposite(uint amount) external reward(msg.sender){
+        require(amount>0,"The amount must be greater than 0");
+        deposit[msg.sender][block.timestamp]=amount;
+        myToken.transfer(address(this), amount);
+        numOfDeposit[msg.sender]+=1;
+  }
 
-//   function withdraw(uint amount)  external reward(msg.sender){
-//     require(amount>0,"The amount must be greater than 0");
+  function withdraw(uint amount)  external reward(msg.sender){
+    require(amount>0,"The amount must be greater than 0");
 
-//     for (uint i = 0; i < numOfDeposit[msg.sender]&&amount!=0; i++) {
-//        uint time = deposit[msg.sender][i];
-//         // Check if the timestamp is valid (not 0) and if it's been more than a week
+    for (uint i = 0; i < numOfDeposit[msg.sender]&&amount!=0; i++) {
+       uint time = deposit[msg.sender][i];
+        // Check if the timestamp is valid (not 0) and if it's been more than a week
         
-//         require(block.timestamp > time + 1 weeks,"Time has not passed");
+        require(block.timestamp > time + 1 weeks,"Time has not passed");
     
-//             uint depositedAmount = deposit[msg.sender][time];
-//             if (amount <= depositedAmount) {
-//                 deposit[msg.sender][time] -= amount;
-//                amount=0;
-//             } 
-//             else {
-//                 deposit[msg.sender][time] = 0;
-//                 amount -= depositedAmount;
-//             }
+            uint depositedAmount = deposit[msg.sender][time];
+            if (amount <= depositedAmount) {
+                deposit[msg.sender][time] -= amount;
+               amount=0;
+            } 
+            else {
+                deposit[msg.sender][time] = 0;
+                amount -= depositedAmount;
+            }
         
         
-//     }
-//     require(amount==0,"you cant withdraw this amount");
-//     uint balanceSupply=myToken.balanceOf(address(this))-totalSupply;
-//     uint percentage = (amount * 100) /  balanceSupply;
-//     myToken.transferFrom(address(this),msg.sender,(totalSupply * percentage)/100);
+    }
+    require(amount==0,"you cant withdraw this amount");
+    uint balanceSupply=myToken.balanceOf(address(this))-totalSupply;
+    uint percentage = (amount * 100) /  balanceSupply;
+    myToken.transferFrom(address(this),msg.sender,(totalSupply * percentage)/100);
     
-//   }
-//   function getBalance() external view returns(uint){
-//     return myToken.balanceOf(address(this));
-//   }
-// }
+  }
+  function getBalance() external view returns(uint){
+    return myToken.balanceOf(address(this));
+  }
+}
 
 
 
